789878defdbcddbe698fcfcb68e24affc04b072c
Carissa Farry <69377985+carissafarry@users.noreply.github.com>
2023-09-06 00:37:31 +0700

    feat(utils): add pendo analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next';
+import Script from 'next/script';
 
 import Providers from '~/app/providers';
 import Layout from '~/lib/layout';
@@ -50,6 +51,21 @@ const RootLayout = ({ children }: RootLayoutProps) => {
           <Layout>{children}</Layout>
         </Providers>
         <Analytics />
+        <Script
+          id="pendo"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(apiKey){
+                (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=o._q||[];
+                  v=['initialize','identify','updateOptions','pageLoad','track'];for(w=0,x=v.length;w<x;++w)(function(m){
+                    o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
+                  y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/'+apiKey+'/pendo.js';
+                  z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
+              })('010e65dc-ce76-4697-47d6-67e316112fec');
+            `,
+          }}
+        />
       </body>
     </html>
   );

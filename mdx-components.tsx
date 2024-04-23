0183c9269e888cf85f398ac2dbd3ac6cf99f2acc
Carissa Farry <carissafarry1@gmail.com>
2024-04-23 16:10:30 +0700

    feat(components): add table of contents prop

--- mdx-components.tsx
+++
@@ -1,8 +0,0 @@
-import type { MDXComponents } from 'mdx/types';
-
-// eslint-disable-next-line import/prefer-default-export
-export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return {
-    ...components,
-  };
-}
